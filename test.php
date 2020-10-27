cdd891e55c32a937f917df9a5887c1b96beda94b
kaiqueluis49 <70417426+kaiqueluis49@users.noreply.github.com>
2020-10-27 09:49:34 -0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+$channel="UC-9-kyTW8ZkZNDHQJ6FgpwQ"; //youtube music channel id
+$Youtube = new Youtube($channel);//Channel id or channel user
+$video   = $Youtube->video(3); //Here you choose the number of videos to be processed
+for($i=0;$i<$video['qtde'];$i++)
+{
+    echo'<h1>'.$video['titulo'][$i].'</h1><br>';
+    echo'<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$video['video'][$i].'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+}
+?>
